194d5ef52dfe542717ce30d25a14339eb0694a1c
Damian <d.rousseau-pro@protonmail.com>
2025-10-07 23:29:56 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,17 +4,15 @@ const auth = (...allowedRoles) => {
     return (req, res, next) => {
         const token = req.header('Authorization')?.replace('Bearer ', '');
         
-        if (!token) {
+        if (!token) 
             return res.status(401).json({ error: 'Access denied' });
-        }
 
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = decoded;
 
-            if (allowedRoles.length && !allowedRoles.includes(decoded.role)) {
-                return res.status(403).json({ error: 'Access denied' });
-            }
+            if (!allowedRoles.includes(decoded.role))
+                return res.status(403).json({ error: decoded });
 
             next();
         } catch (error) {
