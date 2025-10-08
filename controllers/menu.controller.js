4f1b86d6ecb3128b2cf7485d4f2e62c2f2e4d376
Damian <d.rousseau-pro@protonmail.com>
2025-10-08 15:12:32 +0200

    Update fonctionality

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose")
 const Menus = require("../models/menu.model")
 
 exports.getMenus = async (req, res) => {
@@ -7,5 +8,83 @@ exports.getMenus = async (req, res) => {
         res.status(200).json(menus)
     } catch (error) {
         res.status(500).json({ error: "An error occurred while fetching the menus" })
+    }
+}
+
+exports.getMenu = async (req, res) => {
+    try {
+        const id = req.params.id
+        
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({error: 'ID not valid'})
+
+        const menu = await Menus.findById(id)
+
+        res.status(200).json(menu)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while fetching the menu" })
+    }
+}
+
+exports.createMenu = async (req, res) => {
+    try {
+        const { name, description, products } = req.body
+
+        if(!name || !description || !products)
+            return res.status(400).json({error: 'Please fill in the required fields'})
+
+        const menu = new Menus({
+            name,
+            description,
+            products
+        })
+    
+        const savedMenu = await menu.save()
+    
+        res.status(200).json(savedMenu)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while create the menu" })
+    }
+}
+
+exports.editMenu = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, description, products } = req.body
+        const menu = await Menus.findById(id)
+
+        if(!menu)
+            return res.status(404).json({error: 'Product not find'})
+
+        if(name)
+            menu.name = name
+
+        if(description)
+            menu.description = description
+
+        if(products)
+            menu.products = products
+
+        const editMenu = await menu.save()
+
+        res.json(editMenu)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while edit the menu" })
+    }
+}
+
+exports.deleteMenu = async (req, res) => {
+    try {
+        const { id } = req.params
+        const menu = await Menus.findById(id)
+
+        if(!menu)
+            return res.status(404).json({error: 'Menu not find'})
+
+        await menu.deleteOne()
+
+        res.json({message: 'Menu deleted'})
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while delete the menu" })
     }
 }
--- routes/menu.routes.js
+++ routes/menu.routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const { getMenus } = require('../controllers/menu.controller')
+const { getMenus, createMenu, getMenu, editMenu, deleteMenu } = require('../controllers/menu.controller')
 const router = express.Router()
 
 router.get('/', getMenus)
+router.get('/:id', getMenu)
+router.post('/', createMenu)
+router.put('/:id', editMenu)
+router.delete('/:id', deleteMenu)
 
 module.exports = router
