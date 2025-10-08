f5843dfac22985f7225cd011a51f59dc1aadb82f
Damian <d.rousseau-pro@protonmail.com>
2025-10-08 15:31:07 +0200

    Update features

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose")
 const Orders = require("../models/order.model")
 
 exports.getOrders = async (req, res) => {
@@ -7,5 +8,97 @@ exports.getOrders = async (req, res) => {
         res.status(200).json(orders)
     } catch (error) {
         res.status(500).json({ error: "An error occurred while fetching the orders" })
+    }
+}
+
+exports.getOrder = async (req, res) => {
+    try {
+        const id = req.params.id
+                
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({error: 'ID not valid'})
+
+        const order = await Orders.findById(id)
+        
+        res.status(200).json(order)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while fetching the order" })
+    }
+}
+
+exports.createOrder = async (req, res) => {
+    try {
+        const { status, menus } = req.body
+
+        if(!status || !menus)
+            return res.status(400).json({error: 'Please fill in the required fields'})
+
+        const order = new Orders({
+            status,
+            menus
+        })
+    
+        const savedOrder = await order.save()
+    
+        res.status(200).json(savedOrder)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while create the order" })
+    }
+}
+
+exports.finishOrder = async (req, res) => {
+    try {
+        const id = req.params.id
+                
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({error: 'ID not valid'})
+
+        const order = await Orders.findById(id)
+
+        order.status = 'finished'
+
+        const editOrder = await order.save()
+        
+        res.status(200).json(editOrder)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while deliver the order" })
+    }
+}
+
+exports.prepareOrder = async (req, res) => {
+    try {
+        const id = req.params.id
+                
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({error: 'ID not valid'})
+
+        const order = await Orders.findById(id)
+
+        order.status = 'prepared'
+
+        const editOrder = await order.save()
+        
+        res.status(200).json(editOrder)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while deliver the order" })
+    }
+}
+
+exports.deliverOrder = async (req, res) => {
+    try {
+        const id = req.params.id
+                
+        if(!mongoose.Types.ObjectId.isValid(id))
+            return res.status(400).json({error: 'ID not valid'})
+
+        const order = await Orders.findById(id)
+
+        order.status = 'delivred'
+
+        const editOrder = await order.save()
+        
+        res.status(200).json(editOrder)
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred while deliver the order" })
     }
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const { getOrders } = require('../controllers/order.controller')
+const { getOrders, getOrder, createOrder, deliverOrder } = require('../controllers/order.controller')
 const router = express.Router()
 
 router.get('/', getOrders)
+router.get('/:id', getOrder)
+router.post('/', createOrder)
+router.put('/:id/finish', finishOrder)
+router.put('/:id/prepare', prepareOrder)
+router.put('/:id/deliver', deliverOrder)
 
 module.exports = router
