216f113be9a86db72ef7e690a6a50db761d04e61
Damian <d.rousseau-pro@protonmail.com>
2025-11-10 11:02:36 +0100

    Correction for vercel

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const connectDB = require('./config/database.js')
 const app = require('./app')
 const dotenv = require('dotenv')
-const PORT = process.env.PORT
 
 dotenv.config()
+const PORT = process.env.PORT
+
 connectDB()
 
-// app.listen(PORT, () => {
-//   console.log(`Serveur démarré sur le port ${PORT}`)
-// })
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`)
+})
