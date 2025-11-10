ec6a66b3aa5c5b36a3c9d2e75a3ab98e29997dde
Damian <d.rousseau-pro@protonmail.com>
2025-11-10 10:19:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const connectDB = require('./config/database.js')
 const app = require('./app')
 const dotenv = require('dotenv')
-const cors = require('cors')
 const PORT = process.env.PORT
 
 dotenv.config()
 connectDB()
 
-app.use(cors())
-app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Serveur démarré sur le port ${PORT}`)
+// })
+
+module.exports = app
